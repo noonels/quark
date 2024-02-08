1adeafd27c1ac1cdb5ad1c6fc1f80a7c0e055d2a
Cooper Healy <m.cooper.healy@gmail.com>
2024-02-08 15:23:41 -0600

    Steal some prime config

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
+
+-- quickly insert the Go mantra
+vim.keymap.set(
+    "n",
+    "<leader>ige",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,14 +4,29 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = "nvim-lua/plenary.nvim",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>pf", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
-      { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-      { "<leader>o", "<cmd>Telescope oldfiles<cr>", desc = "Old Files" },
-      { "<leader>gk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
-      { "<leader>pp", ":lua require'telescope'.extensions.project.project{}<CR>", desc = "Projects" },
-    },
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        local project = require('telescope').extensions.project
+        local notify = require('telescope').extensions.notify
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>pp', project.project, {})
+        vim.keymap.set('n', '<leader>gk', builtin.keymaps, {})
+        vim.keymap.set('n', '<leader>vn', notify.notify, {})
+    end,
   },
 }
