5a944b9dadd12bb6207b39873057cf61090695e6
M.Cooper Healy <m.cooper.healy@gmail.com>
2024-02-08 21:53:55 -0600

    Add more config stolen from theprimeagen

---
+++ lua/plugins/apm.lua
@@ -0,0 +1,9 @@
+return {
+    "theprimeagen/vim-apm",
+    config = function()
+        local apm = require("vim-apm")
+
+        apm:setup({})
+        vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+    end
+    }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,23 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
- },
+    {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup({
+                icons = true,
+            })
+
+            vim.keymap.set("n", "<leader>tt", function()
+                require("trouble").toggle()
+            end)
+
+            vim.keymap.set("n", "[t", function()
+                require("trouble").next({skip_groups = true, jump = true});
+            end)
+
+            vim.keymap.set("n", "]t", function()
+                require("trouble").previous({skip_groups = true, jump = true});
+            end)
+
+        end
+    },
 }
