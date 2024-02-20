4468eacc4b9d9cc467e71ccf0e19a5153bf2c11e
M.Cooper Healy <m.cooper.healy@gmail.com>
2024-02-19 20:47:00 -0600

    Re-enable DAP

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,44 +1,43 @@
--- return {
---     "mfussenegger/nvim-dap",
---     dependencies = {
---         {
---             "rcarriga/nvim-dap-ui",
---             config = function(_, opts)
---                 local dap = require("dap")
---                 local dapui = require("dapui")
---                 dapui.setup(opts)
---                 dap.listeners.after.event_initialized["dapui_config"] = function()
---                     dapui.open({})
---                 end
---                 dap.listeners.before.event_terminated["dapui_config"] = function()
---                     dapui.close({})
---                 end
---                 dap.listeners.before.event_exited["dapui_config"] = function()
---                     dapui.close({})
---                 end
---             end,
---         },
---         {
---             "suketa/nvim-dap-ruby",
---             config = function()
---                 require("dap-ruby").setup()
---             end,
---         },
---         {
---             "theHamsta/nvim-dap-virtual-text",
---             opts = {},
---         },
---         {
---             "jay-babu/mason-nvim-dap.nvim",
---             dependencies = "mason.nvim",
---             cmd = { "DapInstall", "DapUninstall" },
---             opts = {
---                 automatic_installation = true,
---                 handlers = {},
---                 ensure_installed = {},
---             },
---         },
---         { "jbyuki/one-small-step-for-vimkind", module = "osv" },
---     },
--- }
---
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		{
+			"rcarriga/nvim-dap-ui",
+			config = function(_, opts)
+				local dap = require("dap")
+				local dapui = require("dapui")
+				dapui.setup(opts)
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open({})
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close({})
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close({})
+				end
+			end,
+		},
+		{
+			"suketa/nvim-dap-ruby",
+			config = function()
+				require("dap-ruby").setup()
+			end,
+		},
+		{
+			"theHamsta/nvim-dap-virtual-text",
+			opts = {},
+		},
+		{
+			"jay-babu/mason-nvim-dap.nvim",
+			dependencies = "mason.nvim",
+			cmd = { "DapInstall", "DapUninstall" },
+			opts = {
+				automatic_installation = true,
+				handlers = {},
+				ensure_installed = {},
+			},
+		},
+		{ "jbyuki/one-small-step-for-vimkind", module = "osv" },
+	},
+}
