11fc311414a55d751be19ac5f5a57c86b6d629d2
Cooper <cooper.healy@nabis.com>
2024-03-16 00:00:41 -0500

    Fix lsp bindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,9 +20,6 @@ return {
 		lsp_zero.on_attach(function(client, bufnr)
 			local opts = { buffer = bufnr, remap = false }
 
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
 			vim.keymap.set("n", "K", function()
 				vim.lsp.buf.hover()
 			end, opts)
