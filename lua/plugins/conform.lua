469d7f2856a21067385f0c3b414e08058778a756
Cooper <cooper.healy@nabis.com>
2024-03-27 13:18:15 -0500

    Use a linter we actually have a config for

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,13 +5,13 @@ return {
         require("conform").setup({
             format_on_save = {
                 timeout_ms = 500,
-                lsp_fallback = false,
+                lsp_fallback = true,
             },
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "isort", "black" },
-                javascript = { { "prettierd", "prettier" } },
-                typescript = { { "eslint", "prettierd", "prettier" } },
+                javascript = { { "prettier" } },
+                typescript = { { "eslint_d", "prettier" } },
                 rust = { "rustfmt" },
                 go = { "gofmt", "goimports", "golines" },
                 gleam = { "gleam" },
