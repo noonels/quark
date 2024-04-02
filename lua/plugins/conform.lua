91fe1ebb676e725826af2ede34c07f8810dfde10
M.Cooper Healy <m.cooper.healy@gmail.com>
2024-04-01 23:24:18 -0500

    Fix debugging

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {},
-    config = function()
-        require("conform").setup({
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                javascript = { { "prettier" } },
-                typescript = { { "eslint_d", "prettier" } },
-                rust = { "rustfmt" },
-                go = { "gofmt", "goimports", "golines" },
-                gleam = { "gleam" },
-            },
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                require("conform").format({ bufnr = args.buf })
-            end,
-        })
-    end,
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { { "prettier" } },
+				typescript = { { "eslint_d", "prettier" } },
+				rust = { "rustfmt" },
+				go = { "gofmt", "goimports", "golines" },
+				gleam = { "gleam" },
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,43 +1,34 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		{
-			"rcarriga/nvim-dap-ui",
-			config = function(_, opts)
-				local dap = require("dap")
-				local dapui = require("dapui")
-				dapui.setup(opts)
-				dap.listeners.after.event_initialized["dapui_config"] = function()
-					dapui.open({})
-				end
-				dap.listeners.before.event_terminated["dapui_config"] = function()
-					dapui.close({})
-				end
-				dap.listeners.before.event_exited["dapui_config"] = function()
-					dapui.close({})
-				end
-			end,
-		},
-		{
-			"suketa/nvim-dap-ruby",
-			config = function()
-				require("dap-ruby").setup()
-			end,
-		},
-		{
-			"theHamsta/nvim-dap-virtual-text",
-			opts = {},
-		},
-		{
-			"jay-babu/mason-nvim-dap.nvim",
-			dependencies = "mason.nvim",
-			cmd = { "DapInstall", "DapUninstall" },
-			opts = {
-				automatic_installation = true,
-				handlers = {},
-				ensure_installed = {},
-			},
-		},
-		{ "jbyuki/one-small-step-for-vimkind", module = "osv" },
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+		"leoluz/nvim-dap-go",
 	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		-- auto-setup
+		require("nvim-dap-virtual-text").setup()
+		require("dap-go").setup()
+
+		-- listeners for dapui
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open({})
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open({})
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close({})
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close({})
+		end
+
+		-- keybindings
+		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+	end,
 }
